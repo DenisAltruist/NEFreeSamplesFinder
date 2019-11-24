52f68dae5b1a26ca3ddb2f413b68c64f9855fe36
altruist <anishchenko1999@gmail.com>
2019-11-24 16:59:21 +0300

    flush

--- lp_solver.py
+++ lp_solver.py
@@ -5,7 +5,7 @@ import numpy as np
 def solve(A, b):
   c = np.zeros(len(A[0])).tolist()
   res = linprog(c=c, A_ub=A, b_ub=b)
-  return res['success'], res['x'].tolist()
+  return res['success'], np.array(res['x']).tolist()
   
 def is_feasible(A, b):
   res, _ = solve(A, b)
